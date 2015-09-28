72c4aa532530f2749dc72730fbd851e8d9cc6216
Rudy Matela <rudy@matela.com.br>
2015-09-28 16:40:19 +0100

    Utils: add some haddock, export explicitly

--- Test/Check/Utils.hs
+++ Test/Check/Utils.hs
@@ -1,9 +1,30 @@
 module Test.Check.Utils
+  ( lsNoDupListsOf
+  , lsNoDecListsOf
+  , listingsOfLength
+  , functionsOn
+  , lsPartialFunctions
+  , lsFunctions
+  , partialFunctions
+  , bindingsToFunction'
+  , totalBindingsToFunction
+  )
 where
 
 import Test.Check
 import Data.Maybe (fromMaybe)
 
+-- | Given a listing of values, returns a listing of lists of no repeated
+-- elements.
+--
+-- > lsNoDupListsOf [[0],[1],[2],...] ==
+-- >   [ [[]]
+-- >   , [[0]]
+-- >   , [[1]]
+-- >   , [[0,1],[1,0],[2]]
+-- >   , [[0,2],[2,0],[3]]
+-- >   , ...
+-- >   ]
 lsNoDupListsOf :: [[a]] -> [[[a]]]
 lsNoDupListsOf = ([[]]:) . lsConcat . djsWith (\x xss -> lsmap (x:) (lsNoDupListsOf xss))
 
@@ -24,7 +45,17 @@ djsWith f [[]]         = []
 djsWith f ([]:xss)     = [] : djsWith (\y yss -> f y ([]:yss)) xss
 djsWith f ((x:xs):xss) = [[f x (xs:xss)]] \++/ djsWith (\y (ys:yss) -> f y ((x:ys):yss)) (xs:xss)
 
-
+-- | Given a listing of values, returns a listing of lists of elements in
+-- crescent order (from listing enumeration)
+--
+-- > lsNoDecListsOf [[0],[1],[2],...] ==
+-- >   [ [[]]
+-- >   , [0]
+-- >   , [1]
+-- >   , [[0,1],[2]]
+-- >   , [[0,2],[3]]
+-- >   , [[0,3],[1,2],[4]]
+-- >   , [[0,1,2],[0,4],[1,3],[5]]
 lsNoDecListsOf :: [[a]] -> [[[a]]]
 lsNoDecListsOf = ([[]]:) . lsConcat . ejsWith (\x xss -> lsmap (x:) (lsNoDecListsOf xss))
 
@@ -45,9 +76,9 @@ ejsWith f ([]:xss)     = [] : ejsWith (\y yss -> f y ([]:yss)) xss
 ejsWith f ((x:xs):xss) = [[f x (xs:xss)]] \++/ ejsWith f (xs:xss)
 
 
+-- | Given a listing, returns a listing of lists of a given length.
 listingsOfLength :: Int -> [[a]] -> [[[a]]]
 listingsOfLength n xss = foldr (lsProductWith (:)) [[[]]] (replicate n xss)
-
 
 functionsOn :: [a] -> [[b]] -> [[[(a,b)]]]
 functionsOn xs sbs = lsmap (zip xs) (listingsOfLength (length xs) sbs)
