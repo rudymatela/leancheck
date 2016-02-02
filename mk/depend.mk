3fa1761dc47089004643813e8a6f4d7bbf4f5940
Rudy Matela <rudy@matela.com.br>
2016-02-02 17:42:12 +0000

    Makefile: Update depends with new modules

--- mk/depend.mk
+++ mk/depend.mk
@@ -1,25 +1,27 @@
-tests/test-most.o: tests/test-most.hs Test/Most.hs Test/Check/Derive.hs Test/Operators.hs Test/Check/Utils.hs Test/Check/Invariants.hs Test/Types.hs Test/Check.hs 
-tests/test-most:   tests/test-most.hs Test/Most.hs Test/Check/Derive.hs Test/Operators.hs Test/Check/Utils.hs Test/Check/Invariants.hs Test/Types.hs Test/Check.hs 
-tests/test-derive.o: tests/test-derive.hs Test/Check/Derive.hs Test/Check.hs 
-tests/test-derive:   tests/test-derive.hs Test/Check/Derive.hs Test/Check.hs 
-tests/test-types.o: tests/test-types.hs Test/Types.hs Test/Check.hs 
-tests/test-types:   tests/test-types.hs Test/Types.hs Test/Check.hs 
-tests/test-utils.o: tests/test-utils.hs Test/Check/Invariants.hs Test/Types.hs Test/Check/Utils.hs Test/Check.hs 
-tests/test-utils:   tests/test-utils.hs Test/Check/Invariants.hs Test/Types.hs Test/Check/Utils.hs Test/Check.hs 
-tests/test.o: tests/test.hs Test/Check/Invariants.hs Test/Types.hs Test/Check.hs 
-tests/test:   tests/test.hs Test/Check/Invariants.hs Test/Types.hs Test/Check.hs 
-tests/test-operators.o: tests/test-operators.hs Test/Operators.hs Test/Check.hs 
-tests/test-operators:   tests/test-operators.hs Test/Operators.hs Test/Check.hs 
-Test/Most.o: Test/Most.hs Test/Check/Derive.hs Test/Types.hs Test/Operators.hs Test/Check/Utils.hs Test/Check.hs 
-Test/Check/Invariants.o: Test/Check/Invariants.hs Test/Types.hs Test/Check.hs 
-Test/Check/Function/Periodic.o: Test/Check/Function/Periodic.hs Test/Check.hs 
-Test/Check/Function/CoListable.o: Test/Check/Function/CoListable.hs Test/Check.hs 
-Test/Check/Function/ListsOfPairs.o: Test/Check/Function/ListsOfPairs.hs Test/Check/Utils.hs Test/Check.hs 
-Test/Check/Function/Show.o: Test/Check/Function/Show.hs Test/Check.hs 
-Test/Check/Derive.o: Test/Check/Derive.hs Test/Check.hs 
-Test/Check/Function.o: Test/Check/Function.hs Test/Check/Function/ListsOfPairs.hs Test/Check/Utils.hs Test/Check.hs 
-Test/Check/Utils.o: Test/Check/Utils.hs Test/Check.hs 
-Test/Check/Utils:   Test/Check/Utils.hs Test/Check.hs 
-Test/Check.o: Test/Check.hs 
-Test/Types.o: Test/Types.hs Test/Check.hs 
-Test/Operators.o: Test/Operators.hs Test/Check.hs 
+tests/test-most.o: tests/test-most.hs Test/Most.hs Test/Operators.hs Test/Check/Invariants.hs Test/Types.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+tests/test-most:   tests/test-most.hs Test/Most.hs Test/Operators.hs Test/Check/Invariants.hs Test/Types.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+tests/test-derive.o: tests/test-derive.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+tests/test-derive:   tests/test-derive.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+tests/test-types.o: tests/test-types.hs Test/Types.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+tests/test-types:   tests/test-types.hs Test/Types.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+tests/test-utils.o: tests/test-utils.hs Test/Check/Invariants.hs Test/Types.hs Test/Check.hs Test/Check/Utils.hs Test/Check/Derive.hs Test/Check/Basic.hs Test/Check/Core.hs 
+tests/test-utils:   tests/test-utils.hs Test/Check/Invariants.hs Test/Types.hs Test/Check.hs Test/Check/Utils.hs Test/Check/Derive.hs Test/Check/Basic.hs Test/Check/Core.hs 
+tests/test.o: tests/test.hs Test/Check/Invariants.hs Test/Types.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+tests/test:   tests/test.hs Test/Check/Invariants.hs Test/Types.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+tests/test-operators.o: tests/test-operators.hs Test/Operators.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+tests/test-operators:   tests/test-operators.hs Test/Operators.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Most.o: Test/Most.hs Test/Types.hs Test/Operators.hs Test/Check.hs Test/Check/Utils.hs Test/Check/Derive.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Check/Invariants.o: Test/Check/Invariants.hs Test/Types.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Check/Function/Periodic.o: Test/Check/Function/Periodic.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Check/Function/CoListable.o: Test/Check/Function/CoListable.hs Test/Check.hs Test/Check/Utils.hs Test/Check/Derive.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Check/Function/ListsOfPairs.o: Test/Check/Function/ListsOfPairs.hs Test/Check.hs Test/Check/Utils.hs Test/Check/Derive.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Check/Function/Show.o: Test/Check/Function/Show.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Check/Derive.o: Test/Check/Derive.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Check/Function.o: Test/Check/Function.hs Test/Check/Function/ListsOfPairs.hs Test/Check.hs Test/Check/Utils.hs Test/Check/Derive.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Check/Basic.o: Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Check/Core.o: Test/Check/Core.hs 
+Test/Check/Utils.o: Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Check/Utils:   Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Check.o: Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Types.o: Test/Types.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
+Test/Operators.o: Test/Operators.hs Test/Check.hs Test/Check/Derive.hs Test/Check/Utils.hs Test/Check/Basic.hs Test/Check/Core.hs 
