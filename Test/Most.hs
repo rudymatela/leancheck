3a66c731e485abff34a434bc53aac425962e89fb
Rudy Matela <rudy@matela.com.br>
2016-02-02 21:36:35 +0000

    Improve Test.Most

--- Test/Most.hs
+++ Test/Most.hs
@@ -1,23 +1,19 @@
--- | Very simple property-based testing library based on enumeration of values
---   via lists of lists.
+-- | Simple property-based testing library
+--   based on enumeration of values via lists of lists.
 --
 -- This module exports Most modules that accompain Test.Check
 -- and is to be used as a shorthand:
 --
--- import Test.Most
+-- > import Test.Most
 --
 -- To get most of the needed stuff
 module Test.Most
   ( module Test.Check
-  , module Test.Check.Utils
   , module Test.Operators
   , module Test.Types
---, module Test.Check.Derive
   )
 where
 
 import Test.Check
-import Test.Check.Utils
 import Test.Operators
 import Test.Types
---import Test.Check.Derive
