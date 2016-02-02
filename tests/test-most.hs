d6f2a4142829d02b15568ac632396c794cfcc5b4
Rudy Matela <rudy@matela.com.br>
2016-02-02 21:55:44 +0000

    Fix test-most

--- tests/test-most.hs
+++ tests/test-most.hs
@@ -29,6 +29,6 @@ tests =
   ]
 
 checkCrescent :: Int -> Bool
-checkCrescent n = take n (lsCrescListsOf (listing :: [[Nat]]))
+checkCrescent n = take n (lsStrictlyAscendingListsOf (listing :: [[Nat]]))
                == take n ((map . filter) strictlyOrdered (listing :: [[[Nat]]]))
 
