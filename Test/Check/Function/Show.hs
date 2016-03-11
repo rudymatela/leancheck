3b3804c65fa7d6d05ff488026f9d9fdd1533ae69
Rudy Matela <rudy@matela.com.br>
2016-03-11 14:34:22 +0000

    Document idea about function show

--- Test/Check/Function/Show.hs
+++ Test/Check/Function/Show.hs
@@ -1,5 +1,6 @@
--- | A show instance for functions.
---   Avoid re-exporting this, use only for testing your library.
+-- | An experimental Show instance for functions.
+--
+-- It only works for functions with one argument.
 module Test.Check.Function.Show () where
 
 import Test.Check
@@ -32,3 +33,42 @@ limit3 eq (x:y:z:xs) = if x `eq` y && y `eq` z
 
 functionToPairs :: (Listable a) => (a -> b) -> [(a,b)]
 functionToPairs f = map (\a -> (a,f a)) list
+
+-- Little *rough* idea to implement an instance that works for
+-- functions of N arguments.
+-- (while not depend on special GHC extensions)
+--
+-- We would need a class (similar to ShowMutable of FitSpec):
+--
+-- class ShowFunction where
+--   functionShow :: Int -> FunctionS
+--
+-- Data FunctionS = Atom String | Function String Function | etc...
+--
+-- The default instance for data types would be something like:
+--
+-- functionShowData = Atom . show
+--
+-- For functions:
+--
+-- instance (Show a, ShowFunction b) => ShowFunction (a -> b) where
+--   ...
+--
+--
+-- The actual show instance would be:
+--
+-- instance (Show a, ShowFunction b) => Show (a -> b) where
+--   show = showFunctionS . flatten . showFunction 100
+--
+-- Test.Check.Function.ShowFunction -- Export ShowFunction instance
+-- Test.Check.Function.Show -- Export show instance
+--
+-- To think:
+--
+-- Infinite structure?  With infinite tiers?
+-- Then a flatten infinite structure with simple lists?
+-- Then a function to "crop" it?
+--
+-- Maybe? a default instance for container data types (tuples / lists):
+--
+-- functionShowDataContainer = ...
