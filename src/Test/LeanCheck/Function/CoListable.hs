ff2a4aee61d04df6619388eed91d939bb6ad8106
Rudy Matela <rudy@matela.com.br>
2016-12-12 08:58:15 +0000

    Function.CoListable: rename coListing to cotiers

--- src/Test/LeanCheck/Function/CoListable.hs
+++ src/Test/LeanCheck/Function/CoListable.hs
@@ -8,6 +8,7 @@ where
 
 
 import Test.LeanCheck
+import Test.LeanCheck.Tiers
 import Data.Maybe (fromMaybe)
 
 
@@ -17,51 +18,59 @@ infixr 9 \+:/
 
 
 class CoListable a where
-  coListing :: [[b]] -> [[a -> b]]
+  cotiers :: [[b]] -> [[a -> b]]
 
 
 instance CoListable () where
-  coListing rs = mapT (\r  () -> r) rs
+  cotiers rs = mapT (\r  () -> r) rs
 
 
 instance CoListable Bool where
-  coListing rs = productWith (\r1 r2  b -> if b then r1 else r2) rs rs
+  cotiers rs = productWith (\r1 r2  b -> if b then r1 else r2) rs rs
 
 
 instance CoListable a => CoListable (Maybe a) where
-  coListing rs = productWith (\z f  m -> case m of Nothing -> z
-                                                   Just x  -> f x) rs (coListing rs)
+  cotiers rs = productWith (\z f  m -> case m of
+                                         Nothing -> z
+                                         Just x  -> f x) rs (cotiers rs)
 
 
 instance (CoListable a, CoListable b) => CoListable (Either a b) where
-  coListing rs = productWith (\f g  e -> case e of Left x  -> f x
-                                                   Right x -> g x) (coListing rs) (coListing rs)
+  cotiers rs = productWith (\f g  e -> case e of
+                                         Left x  -> f x
+                                         Right x -> g x) (cotiers rs) (cotiers rs)
 
 
 instance (CoListable a) => CoListable [a] where
-  coListing rss = mapT const rss
-             \+:/ productWith (\y f  xs -> case xs of []      -> y
-                                                      (x:xs') -> f x xs') rss (coListing (coListing rss))
+  cotiers rss = mapT const rss
+           \+:/ productWith
+                  (\y f  xs -> case xs of
+                                 []      -> y
+                                 (x:xs') -> f x xs')
+                  rss
+                  (cotiers (cotiers rss))
+
+
 
 
 instance CoListable Int where
-  coListing rss = mapT const rss
-             \+:/ product3With (\f g z  i -> if i > 0 then f (i-1)
-                                        else if i < 0 then g (i+1)
-                                             else z) (coListing rss) (coListing rss) rss
+  cotiers rss = mapT const rss
+           \+:/ productWith
+                  (\f g  i -> if i >= 0 then f (i-1) else g (i+1))
+                  (cotiers rss) (cotiers rss)
 
 
 alts0 :: [[a]] -> [[a]]
 alts0 = id
 
 alts1 :: CoListable a => [[b]] -> [[a->b]]
-alts1 bs = coListing bs
+alts1 bs = cotiers bs
 
 alts2 :: (CoListable a, CoListable b) => [[c]] -> [[a->b->c]]
-alts2 cs = coListing (coListing cs)
+alts2 cs = cotiers (cotiers cs)
 
 alts3 :: (CoListable a, CoListable b, CoListable c) => [[d]] -> [[a->b->c->d]]
-alts3 ds = coListing (coListing (coListing ds))
+alts3 ds = cotiers (cotiers (cotiers ds))
 
-fListing :: (CoListable a, Listable b) => [[a->b]]
-fListing = coListing tiers
+ftiers :: (CoListable a, Listable b) => [[a->b]]
+ftiers = cotiers tiers
