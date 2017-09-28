e3a83339ef7bc4248c6198b151f0b873b8f4db7a
Rudy Matela <rudy@matela.com.br>
2017-09-28 17:27:39 +0100

    eg/higher-order: add another property

--- eg/higher-order.hs
+++ eg/higher-order.hs
@@ -6,6 +6,10 @@ type A = Int
 prop_foldlFoldr :: (A -> A -> A) -> A -> [A] -> Bool
 prop_foldlFoldr f z xs  =  foldr f z xs == foldl f z xs
 
+prop_foldl1Foldr1Reverse :: (A -> A -> A) -> [A] -> Bool
+prop_foldl1Foldr1Reverse f xs  =
+  not (null xs) ==> foldl1 f xs ==  foldr1 f (reverse xs)
+
 prop_mapFilter :: (A -> A) -> (A -> Bool) -> [A] -> Bool
 prop_mapFilter f p xs  =  filter p (map f xs) == map f (filter p xs)
 
