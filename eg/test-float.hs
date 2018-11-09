26aeaa421025448ef026a01acda58ce27142a8ea
Rudy Matela <rudy@matela.com.br>
2018-11-09 09:29:46 -0300

    add another couple of tests to eg/test-float

--- eg/test-float.hs
+++ eg/test-float.hs
@@ -7,6 +7,7 @@ import Test.LeanCheck
 main :: IO ()
 main = do
   -- identities on floats
+  -- some of these properties fail due to rounding errors
   checkFor n $ \x y -> not (isInfinite y) ==> x - y + y == (x::Float)
   checkFor n $ \x y -> not (isInfinite y) ==> x + y - y == (x::Float)
   checkFor n $ \x y -> y /= 0 && not (isInfinite y) ==> x * y / y == (x::Float)
@@ -17,5 +18,7 @@ main = do
   checkFor n $ \x -> x + x + x + x + x == x * (5::Float)
   checkFor n $ \x -> x + x + x + x + x + x == x * (6::Float)
   checkFor n $ \() -> 0.1 + 0.1 + 0.1 == 0.3
+  checkFor n $ \x -> x >= 0 ==> (x ** 2) ** 0.5 == (x::Float)
+  checkFor n $ \x -> x >= 0 ==> (x ** 0.5) ** 2 == (x::Float)
   where
   n = 10000
