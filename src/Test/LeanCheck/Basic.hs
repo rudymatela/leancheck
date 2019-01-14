3ef1ac2b3cdd10229503b8fd51f2e7cac2aacc5e
Rudy Matela <rudy@matela.com.br>
2019-01-13 21:29:17 -0300

    Update copyright notice year.

--- src/Test/LeanCheck/Basic.hs
+++ src/Test/LeanCheck/Basic.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE CPP #-}
 -- |
 -- Module      : Test.LeanCheck.Basic
--- Copyright   : (c) 2015-2018 Rudy Matela
+-- Copyright   : (c) 2015-2019 Rudy Matela
 -- License     : 3-Clause BSD  (see the file LICENSE)
 -- Maintainer  : Rudy Matela <rudy@matela.com.br>
 --
