355252dfd45b8af3d9a2e062bb760a9cb361a16b
Rudy Matela <rudy@matela.com.br>
2019-02-17 21:40:06 -0300

    fix build on GHC 8.0: change > to >= in CPP

--- test/funshow.hs
+++ test/funshow.hs
@@ -1,7 +1,7 @@
 -- Copyright (c) 2015-2018 Rudy Matela.
 -- Distributed under the 3-Clause BSD licence (see the file LICENSE).
 {-# LANGUAGE CPP #-}
-#if __GLASGOW_HASKELL__ > 800
+#if __GLASGOW_HASKELL__ >= 800
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 #endif
 import System.Exit (exitFailure)
