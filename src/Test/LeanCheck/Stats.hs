b4aebba6096d30b31a05a453aeb62a2860f38f57
Rudy Matela <rudy@matela.com.br>
2020-03-22 17:50:38 -0300

    Document main Stats functions

--- src/Test/LeanCheck/Stats.hs
+++ src/Test/LeanCheck/Stats.hs
@@ -43,6 +43,26 @@ intercalate xs xss = concat (intersperse xs xss)
     prependToAll sep (x:xs) = sep : x : prependToAll sep xs
 #endif
 
+-- | Prints statistics about a given 'Listable' generator
+--   up to a number of test values.
+--
+-- For example,
+-- to know the distribution of the length of generated lists,
+-- just run:
+--
+-- > > classStats 100 (length :: [Int] -> Int)
+-- > 0:  1/100  1%
+-- > 1:  6/100  6%
+-- > 2: 16/100 16%
+-- > 3: 25/100 25%
+-- > 4: 26/100 26%
+-- > 5: 18/100 18%
+-- > 6:  7/100  7%
+-- > 7:  1/100  1%
+--
+-- This provides similar functionality to QuickCheck's
+-- label, collect, classify and tabulate
+-- while keeping statistics separate from your properties.
 classStats :: (Listable a, Show b) => Int -> (a -> b) -> IO ()
 classStats n f = putStrLn
                . table " "
@@ -56,6 +76,29 @@ classStats n f = putStrLn
                     , show (100 * n `div` len) ++ "%"
                     ]
 
+-- | Same as 'classStats', but separated by 'tiers'.
+--
+-- The first argument is the number of tiers to evaluate.
+--
+-- > > classStatsT 6 (length :: [Int] -> Int)
+-- >       tot   0  1  2  3  4   5
+-- > 
+-- > tot:   32   1  1  2  4  8  16
+-- >   0:    1   1  0  0  0  0   0
+-- >   1:    5   0  1  1  1  1   1
+-- >   2:   10   0  0  1  2  3   4
+-- >   3:   10   0  0  0  1  3   6
+-- >   4:    5   0  0  0  0  1   4
+-- >   5:    1   0  0  0  0  0   1
+--
+-- Lines are values, columns are tiers:
+--
+-- > > classStatsT 6 and
+-- >         tot   0  1  2  3   4   5
+-- > 
+-- >   tot:   63   1  2  4  8  16  32
+-- >  True:    6   1  1  1  1   1   1
+-- > False:   57   0  1  3  7  15  31
 classStatsT :: (Listable a, Show b) => Int -> (a -> b) -> IO ()
 classStatsT n f = putStrLn
                 . table "  "
@@ -72,6 +115,18 @@ classStatsT n f = putStrLn
   totalizeCounts = foldr (-+-) (undefined, 0, repeat 0)
   prependTotal cs = totalizeCounts cs : cs
 
+-- | How many values match each of a list of conditions?
+-- 
+-- How many odd and even numbers are in the 'Listable' enumeration?
+--
+-- > > conditionStats 1000 [("odd", odd :: Int -> Bool), ("even", even)]
+-- >  odd: 500/1000 50%
+-- > even: 500/1000 50%
+--
+-- How many of the generated lists are ordered?
+--
+-- > > conditionStats 1000 [("ordered", ordered :: [Int] -> Bool)]
+-- > ordered: 131/1000 13%
 conditionStats :: Listable a => Int -> [(String,a->Bool)] -> IO ()
 conditionStats n = putStrLn . table " " . map show1
   where
@@ -83,6 +138,22 @@ conditionStats n = putStrLn . table " " . map show1
                    , show (100 * c `div` len) ++ "%" ]
   count f = length . filter f
 
+-- | Same as 'conditionStats' but by tier.
+--
+-- The first argument is the number of tiers to evaluate.
+--
+-- How many odd and even numbers are in each tier?
+--
+-- > > conditionStatsT 10 [("odd", odd :: Int -> Bool), ("even", even)]
+-- > total: 1 1 1 1 1 1 1 1 1 1
+-- >   odd: 0 1 1 0 0 1 1 0 0 1
+-- >  even: 1 0 0 1 1 0 0 1 1 0
+--
+-- How many ordered lists are in each tier?
+--
+-- > > conditionStatsT 10 [("ordered", ordered :: [Int] -> Bool)]
+-- >   total: 1 1 2 4 8 16 32 64 128 256
+-- > ordered: 1 1 2 3 5  7 11 15  22  30
 conditionStatsT :: Listable a => Int -> [(String,a->Bool)] -> IO ()
 conditionStatsT n = putStrLn . table " " . map show1 . (("total", const True):)
   where
