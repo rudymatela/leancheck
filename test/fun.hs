5ac91404374e58c1806812a9351fca229c382f5b
Rudy Matela <rudy@matela.com.br>
2020-03-22 15:11:25 -0300

    test/fun: add some tests

--- test/fun.hs
+++ test/fun.hs
@@ -1,24 +1,42 @@
--- Copyright (c) 2015-2018 Rudy Matela.
+-- Copyright (c) 2015-2020 Rudy Matela.
 -- Distributed under the 3-Clause BSD licence (see the file LICENSE).
 import Test
 
 import System.Exit (exitFailure)
 import Data.List (elemIndices)
-
--- import Test.LeanCheck -- already exported by Test
--- import Test.LeanCheck.Utils
-
+import Test.LeanCheck.Function ()
 
 main :: IO ()
 main =
-  case elemIndices False tests of
+  case elemIndices False (tests 200) of
     [] -> putStrLn "Tests passed!"
     is -> do putStrLn ("Failed tests:" ++ show is)
              exitFailure
 
-tests :: [Bool]
-tests =
+tests :: Int -> [Bool]
+tests n =
   [ True
-  -- TODO: add some tests here
-  , holds 100 $ \() -> True
+  , fails n prop_foldlr
+  , holds n prop_foldlr'
+  , fails n prop_mapFilter
+  , fails n prop_false
+  , fails n prop_false'
   ]
+
+type A = Int
+
+prop_foldlr :: (A -> A -> A) -> A -> [A] -> Bool
+prop_foldlr f z xs  =  foldr f z xs == foldl f z xs
+
+-- correct version of the property above
+prop_foldlr' :: (A -> A -> A) -> A -> [A] -> Bool
+prop_foldlr' f z xs  =  foldl (flip f) z (reverse xs) == foldr f z xs
+
+prop_mapFilter :: (A -> A) -> (A -> Bool) -> [A] -> Bool
+prop_mapFilter f p xs  =  filter p (map f xs) == map f (filter p xs)
+
+prop_false :: (A -> A) -> (A -> A) -> Bool
+prop_false _ _ = False
+
+prop_false' :: (A -> A) -> (A,A) -> Bool
+prop_false' _ _ = False
