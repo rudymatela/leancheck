08e67793cac157523b51e26d3fca4d74a9bbbfc8
Rudy Matela <rudy@matela.com.br>
2020-03-25 15:18:03 -0300

    LC.IO: double-space declarations

--- src/Test/LeanCheck/IO.hs
+++ src/Test/LeanCheck/IO.hs
@@ -27,7 +27,7 @@ import Control.Exception (SomeException, catch, evaluate)
 #else
 -- on Hugs
 import Control.Exception (Exception, catch, evaluate)
-type SomeException = Exception
+type SomeException  =  Exception
 #endif
 
 -- | Checks a property printing results on 'System.IO.stdout'
@@ -38,7 +38,7 @@ type SomeException = Exception
 -- > *** Failed! Falsifiable (after 4 tests):
 -- > [] [0,0]
 check :: Testable a => a -> IO ()
-check p = checkResult p >> return ()
+check p  =  checkResult p >> return ()
 
 -- | Check a property for a given number of tests
 --   printing results on 'System.IO.stdout'
@@ -46,7 +46,7 @@ check p = checkResult p >> return ()
 -- > > checkFor 1000 $ \xs -> sort (sort xs) == sort (xs::[Int])
 -- > +++ OK, passed 1000 tests.
 checkFor :: Testable a => Int -> a -> IO ()
-checkFor n p = checkResultFor n p >> return ()
+checkFor n p  =  checkResultFor n p >> return ()
 
 -- | Check a property
 --   printing results on 'System.IO.stdout' and
@@ -63,7 +63,7 @@ checkFor n p = checkResultFor n p >> return ()
 -- There is no option to silence this function:
 -- for silence, you should use 'Test.LeanCheck.holds'.
 checkResult :: Testable a => a -> IO Bool
-checkResult p = checkResultFor 200 p
+checkResult p  =  checkResultFor 200 p
 
 -- | Check a property for a given number of tests
 --   printing results on 'System.IO.stdout' and
@@ -76,56 +76,57 @@ checkResult p = checkResultFor 200 p
 -- There is no option to silence this function:
 -- for silence, you should use 'Test.LeanCheck.holds'.
 checkResultFor :: Testable a => Int -> a -> IO Bool
-checkResultFor n p = do
+checkResultFor n p  =  do
   r <- resultIO n p
   putStrLn . showResult n $ r
   return (isOK r)
-  where isOK (OK _) = True
-        isOK _      = False
+  where
+  isOK (OK _)  =  True
+  isOK _       =  False
 
-data Result = OK        Int
-            | Falsified Int [String]
-            | Exception Int [String] String
+data Result  =  OK        Int
+             |  Falsified Int [String]
+             |  Exception Int [String] String
   deriving (Eq, Show)
 
 resultsIO :: Testable a => Int -> a -> [IO Result]
-resultsIO n = zipWith torio [1..] . take n . results
+resultsIO n  =  zipWith torio [1..] . take n . results
   where
-    tor i (_,True) = OK i
-    tor i (as,False) = Falsified i as
-    torio i r@(as,_) = evaluate (tor i r)
-       `catch` \e -> let _ = e :: SomeException
-                     in return (Exception i as (show e))
+  tor i (_,True)  =  OK i
+  tor i (as,False)  =  Falsified i as
+  torio i r@(as,_)  =  evaluate (tor i r)
+     `catch` \e -> let _  =  e :: SomeException
+                   in return (Exception i as (show e))
 
 resultIO :: Testable a => Int -> a -> IO Result
-resultIO n = computeResult . resultsIO n
+resultIO n  =  computeResult . resultsIO n
   where
-  computeResult []  = error "resultIO: no results, empty Listable enumeration?"
-  computeResult [r] = r
-  computeResult (r:rs) = r >>= \r -> case r of
-                                     (OK _) -> computeResult rs
-                                     _      -> return r
+  computeResult []   =  error "resultIO: no results, empty Listable enumeration?"
+  computeResult [r]  =  r
+  computeResult (r:rs)  =  r >>= \r -> case r of
+                                       (OK _) -> computeResult rs
+                                       _      -> return r
 
 showResult :: Int -> Result -> String
-showResult m (OK n)             = "+++ OK, passed " ++ show n ++ " tests"
-                               ++ takeWhile (\_ -> n < m) " (exhausted)" ++ "."
-showResult m (Falsified i ce)   = "*** Failed! Falsifiable (after "
-                               ++ show i ++ " tests):\n" ++ joinArgs ce
-showResult m (Exception i ce e) = "*** Failed! Exception '" ++ e ++ "' (after "
-                               ++ show i ++ " tests):\n" ++ joinArgs ce
+showResult m (OK n)              =  "+++ OK, passed " ++ show n ++ " tests"
+                                 ++ takeWhile (\_ -> n < m) " (exhausted)" ++ "."
+showResult m (Falsified i ce)    =  "*** Failed! Falsifiable (after "
+                                 ++ show i ++ " tests):\n" ++ joinArgs ce
+showResult m (Exception i ce e)  =  "*** Failed! Exception '" ++ e ++ "' (after "
+                                 ++ show i ++ " tests):\n" ++ joinArgs ce
 
 -- joins the counter-example arguments
 joinArgs :: [String] -> String
-joinArgs ce | any ('\n' `elem`) ce = unlines $ map (chopBreak . deparenf) ce
-            | otherwise            = unwords ce
+joinArgs ce | any ('\n' `elem`) ce  =  unlines $ map (chopBreak . deparenf) ce
+            | otherwise             =  unwords ce
 
 -- deparenthises a functional expression if it is parenthized
 deparenf :: String -> String
-deparenf ('(':'\\':cs) | last cs == ')' = '\\':init cs
-deparenf cs                        = cs
+deparenf ('(':'\\':cs) | last cs == ')'  =  '\\':init cs
+deparenf cs                              =  cs
 
 -- chops a line break at the end if there is any
 chopBreak :: String -> String
-chopBreak [] = []
-chopBreak ['\n'] = []
-chopBreak (x:xs) = x:chopBreak xs
+chopBreak []      =  []
+chopBreak ['\n']  =  []
+chopBreak (x:xs)  =  x:chopBreak xs
