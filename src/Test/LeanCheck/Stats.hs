151615ee3db318b27c05197299afa3aef003bfdf
Rudy Matela <rudy@matela.com.br>
2020-03-25 15:22:33 -0300

    LC.Stats: double-space declarations

--- src/Test/LeanCheck/Stats.hs
+++ src/Test/LeanCheck/Stats.hs
@@ -32,15 +32,15 @@ import Data.List (intercalate, transpose)
 import Data.List (transpose)
 
 intercalate :: [a] -> [[a]] -> [a]
-intercalate xs xss = concat (intersperse xs xss)
+intercalate xs xss  =  concat (intersperse xs xss)
   where
-  intersperse             :: a -> [a] -> [a]
-  intersperse _   []      = []
-  intersperse sep (x:xs)  = x : prependToAll sep xs
+  intersperse :: a -> [a] -> [a]
+  intersperse _ []        =  []
+  intersperse sep (x:xs)  =  x : prependToAll sep xs
     where
-    prependToAll            :: a -> [a] -> [a]
-    prependToAll _   []     = []
-    prependToAll sep (x:xs) = sep : x : prependToAll sep xs
+    prependToAll :: a -> [a] -> [a]
+    prependToAll _   []      =  []
+    prependToAll sep (x:xs)  =  sep : x : prependToAll sep xs
 #endif
 
 -- | Prints statistics about a given 'Listable' generator
@@ -64,17 +64,17 @@ intercalate xs xss = concat (intersperse xs xss)
 -- label, collect, classify and tabulate
 -- while keeping statistics separate from your properties.
 classStats :: (Listable a, Show b) => Int -> (a -> b) -> IO ()
-classStats n f = putStrLn
-               . table " "
-               . map showCount
-               $ countsOn (unquote . show . f) xs
+classStats n f  =  putStrLn
+                .  table " "
+                .  map showCount
+                $  countsOn (unquote . show . f) xs
   where
-  xs = take n list
-  len = length xs
-  showCount (s,n) = [ s ++ ":"
-                    , show n ++ "/" ++ show len
-                    , show (100 * n `div` len) ++ "%"
-                    ]
+  xs  =  take n list
+  len  =  length xs
+  showCount (s,n)  =  [ s ++ ":"
+                      , show n ++ "/" ++ show len
+                      , show (100 * n `div` len) ++ "%"
+                      ]
 
 -- | Same as 'classStats', but separated by 'tiers'.
 --
@@ -100,20 +100,20 @@ classStats n f = putStrLn
 -- >  True:    6   1  1  1  1   1   1
 -- > False:   57   0  1  3  7  15  31
 classStatsT :: (Listable a, Show b) => Int -> (a -> b) -> IO ()
-classStatsT n f = putStrLn
-                . table "  "
-                . (heading:)
-                . ([" "]:)
-                . map showCounts
-                . prependTotal
-                . countsTOn (unquote . show . f)
-                $ take n tiers
+classStatsT n f  =  putStrLn
+                 .  table "  "
+                 .  (heading:)
+                 .  ([" "]:)
+                 .  map showCounts
+                 .  prependTotal
+                 .  countsTOn (unquote . show . f)
+                 $  take n tiers
   where
-  heading = "" : "tot " : map show [0..(n-1)]
-  showCounts (s,n,ns) = (s ++ ":") : (show n ++ " ") : map show ns
-  (_,n,ns) -+- (_,n',ns') = ("tot", n + n', zipWith (+) ns ns')
-  totalizeCounts = foldr (-+-) (undefined, 0, repeat 0)
-  prependTotal cs = totalizeCounts cs : cs
+  heading  =  "" : "tot " : map show [0..(n-1)]
+  showCounts (s,n,ns)  =  (s ++ ":") : (show n ++ " ") : map show ns
+  (_,n,ns) -+- (_,n',ns')  =  ("tot", n + n', zipWith (+) ns ns')
+  totalizeCounts  =  foldr (-+-) (undefined, 0, repeat 0)
+  prependTotal cs  =  totalizeCounts cs : cs
 
 -- | How many values match each of a list of conditions?
 -- 
@@ -128,15 +128,15 @@ classStatsT n f = putStrLn
 -- > > conditionStats 1000 [("ordered", ordered :: [Int] -> Bool)]
 -- > ordered: 131/1000 13%
 conditionStats :: Listable a => Int -> [(String,a->Bool)] -> IO ()
-conditionStats n = putStrLn . table " " . map show1
+conditionStats n  =  putStrLn . table " " . map show1
   where
-  xs = take n list
-  len = length xs
-  show1 (s,f) = let c = count f xs
-                in [ s ++ ":"
-                   , show c ++ "/" ++ show len
-                   , show (100 * c `div` len) ++ "%" ]
-  count f = length . filter f
+  xs  =  take n list
+  len  =  length xs
+  show1 (s,f)  =  let c = count f xs
+                  in [ s ++ ":"
+                     , show c ++ "/" ++ show len
+                     , show (100 * c `div` len) ++ "%" ]
+  count f  =  length . filter f
 
 -- | Same as 'conditionStats' but by tier.
 --
@@ -155,11 +155,11 @@ conditionStats n = putStrLn . table " " . map show1
 -- >   total: 1 1 2 4 8 16 32 64 128 256
 -- > ordered: 1 1 2 3 5  7 11 15  22  30
 conditionStatsT :: Listable a => Int -> [(String,a->Bool)] -> IO ()
-conditionStatsT n = putStrLn . table " " . map show1 . (("total", const True):)
+conditionStatsT n  =  putStrLn . table " " . map show1 . (("total", const True):)
   where
-  xss = take n tiers
-  show1 (s,f) = (s ++ ":") : map (show . count f) xss
-  count f = length . filter f
+  xss  =  take n tiers
+  show1 (s,f)  =  (s ++ ":") : map (show . count f) xss
+  count f  =  length . filter f
 
 -- | Classify values using their 'Eq' instance.
 --
@@ -168,7 +168,7 @@ conditionStatsT n = putStrLn . table " " . map show1 . (("total", const True):)
 --
 -- (cf. 'classifyBy', 'classifyOn')
 classify :: Eq a => [a] -> [[a]]
-classify = classifyBy (==)
+classify  =  classifyBy (==)
 
 -- | Classify values by a given comparison function.
 --
@@ -177,11 +177,11 @@ classify = classifyBy (==)
 --
 -- (cf. 'classify', 'classifyOn')
 classifyBy :: (a -> a -> Bool) -> [a] -> [[a]]
-classifyBy (==) []     = []
-classifyBy (==) (x:xs) = (x:filter (== x) xs)
+classifyBy (==) []      =  []
+classifyBy (==) (x:xs)  =  (x:filter (== x) xs)
                        : classifyBy (==) (filter (/= x) xs)
   where
-  x /= y = not (x == y)
+  x /= y  =  not (x == y)
 
 -- | Classify values based on the result of a given function.
 --
@@ -193,7 +193,7 @@ classifyBy (==) (x:xs) = (x:filter (== x) xs)
 --
 -- (cf. 'classify', 'classifyBy')
 classifyOn :: Eq b => (a -> b) -> [a] -> [[a]]
-classifyOn f xs = map (map fst)
+classifyOn f xs  =  map (map fst)
                 . classifyBy ((==) `on` snd)
                 $ map (\x -> (x,f x)) xs
 
@@ -204,12 +204,12 @@ classifyOn f xs = map (map fst)
 --
 -- Values are returned in the order they appear.
 counts :: Eq a => [a] -> [(a,Int)]
-counts = map headLength . classify
+counts  =  map headLength . classify
 
 -- | Returns the counts of each value in a list
 --   using a given comparison function.
 countsBy :: (a -> a -> Bool) -> [a] -> [(a,Int)]
-countsBy (==) = map headLength . classifyBy (==)
+countsBy (==)  =  map headLength . classifyBy (==)
 
 -- | Returns the counts of each value in a list
 --   based on a projection.
@@ -217,34 +217,34 @@ countsBy (==) = map headLength . classifyBy (==)
 -- > > countsOn length ["sheep", "chip", "ship", "cheap", "Mississippi"]
 -- > [(5,2),(4,2),(11,1)]
 countsOn :: Eq b => (a -> b) -> [a] -> [(b,Int)]
-countsOn f = map (\xs -> (f $ head xs, length xs)) . classifyOn f
+countsOn f  =  map (\xs -> (f $ head xs, length xs)) . classifyOn f
 
 countsT :: Eq a => [[a]] -> [(a,Int,[Int])]
-countsT xss = [(x,n,map (count x) xss) | (x,n) <- counts (concat xss)]
+countsT xss  =  [(x,n,map (count x) xss) | (x,n) <- counts (concat xss)]
   where
-  count x = length . filter (== x)
+  count x  =  length . filter (== x)
 
 countsTOn :: Eq b => (a -> b) -> [[a]] -> [(b,Int,[Int])]
-countsTOn f = countsT . mapT f
+countsTOn f  =  countsT . mapT f
 
 headLength :: [a] -> (a,Int)
-headLength xs = (head xs, length xs)
+headLength xs  =  (head xs, length xs)
 
 unquote :: String -> String
-unquote ('"':s) | last s == '"' = init s
-unquote s = s
+unquote ('"':s) | last s == '"'  =  init s
+unquote s  =  s
 
 table :: String -> [[String]] -> String
-table s []  = ""
-table s sss = unlines
-            . map (removeTrailing ' ')
-            . map (intercalate s)
-            . transpose
-            . map (normalize ' ')
-            . foldr1 (zipWith (++))
-            . map (normalize "" . map lines)
-            . normalize ""
-            $ sss
+table s []   =  ""
+table s sss  =  unlines
+             .  map (removeTrailing ' ')
+             .  map (intercalate s)
+             .  transpose
+             .  map (normalize ' ')
+             .  foldr1 (zipWith (++))
+             .  map (normalize "" . map lines)
+             .  normalize ""
+             $  sss
 
 -- | Fits a list to a certain width by appending a certain value
 --
@@ -252,19 +252,19 @@ table s sss = unlines
 --
 -- > fit 0 6 [1,2,3] == [1,2,3,0,0,0]
 fit :: a -> Int -> [a] -> [a]
-fit x n xs = replicate (n - length xs) x ++ xs
+fit x n xs  =  replicate (n - length xs) x ++ xs
 
 -- | normalize makes all list the same length by adding a value
 --
 -- > normalize ["asdf","qw","er"] == normalize ["asdf","qw  ","er  "]
 normalize :: a -> [[a]] -> [[a]]
-normalize x xs = map (x `fit` maxLength xs) xs
+normalize x xs  =  map (x `fit` maxLength xs) xs
 
 -- | Given a list of lists returns the maximum length
 maxLength :: [[a]] -> Int
-maxLength = maximum . (0:) . map length
+maxLength  =  maximum . (0:) . map length
 
 removeTrailing :: Eq a => a -> [a] -> [a]
-removeTrailing x = reverse
-                 . dropWhile (==x)
-                 . reverse
+removeTrailing x  =  reverse
+                  .  dropWhile (==x)
+                  .  reverse
