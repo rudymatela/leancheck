4ad68db08c31974b0b8a7a57818db315adfd5b66
Rudy Matela <rudy@matela.com.br>
2021-07-02 18:27:22 -0300

    import Data.Function backport for Hugs
    better copyright notice and use ? for the operator argument

--- etc/hugs-backports/Data/Function.hs
+++ etc/hugs-backports/Data/Function.hs
@@ -1,12 +1,13 @@
--- Copyright 2018-2020 Rudy Matela
---
 -- Backport of Data.Function for Hugs 2006-09.
 -- Only exports `on`
+--
+-- Copyright (c) 2018-2021 Rudy Matela.
+-- Distributed under the 3-Clause BSD licence (see the file LICENSE).
 module Data.Function
   ( on
   )
 where
 
 on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
-(.*.) `on` f = \x y -> f x .*. f y
+(?) `on` f  =  \x y -> f x ? f y
 infixl 0 `on`
