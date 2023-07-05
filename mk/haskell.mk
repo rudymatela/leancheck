f0c97957fa4fd90299852d69128b2d06f34a6c98
Rudy Matela <rudy@matela.com.br>
2023-07-05 21:52:14 +0200

    update haskell.mk

--- mk/haskell.mk
+++ mk/haskell.mk
@@ -90,7 +90,7 @@ depend:
 	find $(ALL_HSS) | ./mk/ghcdeps -i$(GHCIMPORTDIRS) $(GHCFLAGS) > $(DEPMK)
 
 install-dependencies:
-	$(CABAL_INSTALL) --only-dependencies
+	$(CABAL_INSTALL) $(INSTALL_DEPS)
 
 # haddock rules
 haddock: doc/index.html
