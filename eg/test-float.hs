571dae55a8bfc6284c5c59668e69b5ee170e8731
Rudy Matela <rudy@matela.com.br>
2024-01-25 15:27:34 +0100

    test-float: use sqrt

--- eg/test-float.hs
+++ eg/test-float.hs
@@ -18,7 +18,7 @@ main  =  do
   checkFor n $ \x -> x + x + x + x + x == x * (5::Float)
   checkFor n $ \x -> x + x + x + x + x + x == x * (6::Float)
   checkFor n $ \() -> 0.1 + 0.1 + 0.1 == 0.3
-  checkFor n $ \x -> x >= 0 ==> (x ** 2) ** 0.5 == (x::Float)
-  checkFor n $ \x -> x >= 0 ==> (x ** 0.5) ** 2 == (x::Float)
+  checkFor n $ \x -> x >= 0 ==> sqrt (x ^ 2) == (x::Float)
+  checkFor n $ \x -> x >= 0 ==> sqrt x ^ 2 == (x::Float)
   where
   n  =  10000
